d1eef8afb7ea89d4e5910c7bde7844e85bb33699
mnpan <sa69mo@gmail.com>
2017-03-18 15:49:27 +0800

    Triangle Edge: Robust test

--- Triangle/Triangle_Edge.cpp
+++ Triangle/Triangle_Edge.cpp
@@ -23,3 +23,28 @@ TEST( TriEdge, Normal){
 		}
 	}
 }
+
+TEST( TriEdge, Robust){
+	int data[12] = { 0, 1, 2, 66, 67, 68, 133, 134, 135, 199, 200, 201};
+	
+	for( int j = 0; j < 12 ; j++){
+		for( int k = 0; k<12; k++){
+			ASSERT_EQ( OUT_RANGE, Triangle( data[0], data[j], data[k]));
+			ASSERT_EQ( OUT_RANGE, Triangle( data[11], data[j], data[k]));
+		}
+	}
+
+	for( int i = 0; i < 12 ; i++){
+		for( int k = 0; k<12; k++){
+			ASSERT_EQ( OUT_RANGE, Triangle( data[i], data[0], data[k]));
+			ASSERT_EQ( OUT_RANGE, Triangle( data[i], data[11], data[k]));
+		}
+	}	
+
+	for( int i = 0; i < 12 ; i++){
+		for( int j = 0; j<12; j++){
+			ASSERT_EQ( OUT_RANGE, Triangle( data[i], data[j], data[0]));
+			ASSERT_EQ( OUT_RANGE, Triangle( data[i], data[j], data[11]));
+		}
+	}		
+}
