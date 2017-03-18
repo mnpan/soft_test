c146c95f56361ef2ed0120985dc484925d939272
mnpan <sa69mo@gmail.com>
2017-03-18 23:18:05 +0800

    NextDate Boundary Value: WorstCase test 1 failed

--- NextDate/NextDate_BoundaryValueTest.cpp
+++ NextDate/NextDate_BoundaryValueTest.cpp
@@ -24,3 +24,32 @@ TEST( NextBoundary, Normal){
 	ASSERT_EQ( 20110616, NextDate( y[3], m[2], d[2]));
 	ASSERT_EQ( 20120616, NextDate( y[4], m[2], d[2]));
 }
+
+TEST( NextBoundary, WorstCase){
+	/*** min, min+, nor, max-, max ***/
+	int y[5] = { 1812, 1813, 1912, 2011, 2012};
+	int m[5] = { 1, 2, 6, 11, 12};
+	int d[5] = { 1, 2, 15, 30, 31};
+	
+	for( int i = 0; i < 5; i++){
+		for( int j = 0; j < 5; j++){
+			for( int k = 0; k < 3; k++){
+				ASSERT_EQ( (y[i]*10000 + m[j]*100 + d[k] + 1), NextDate( y[i], m[j], d[k]));
+			}
+		}
+		ASSERT_EQ( (y[i]*10000 + m[0]*100 + d[3] + 1), NextDate( y[i], m[0], d[3]));
+		ASSERT_EQ( (y[i]*10000 + (m[0]+1)*100 + 1), NextDate( y[i], m[0], d[4]));
+		
+		ASSERT_EQ( -1, NextDate( y[i], m[1], d[3]));
+		ASSERT_EQ( -1, NextDate( y[i], m[1], d[4]));
+		
+		ASSERT_EQ( (y[i]*10000 + m[2]*100 + d[3] + 1), NextDate( y[i], m[2], d[3]));
+		ASSERT_EQ( -1, NextDate( y[i], m[2], d[4]));
+		
+		ASSERT_EQ( (y[i]*10000 + m[3]*100 + d[3] + 1), NextDate( y[i], m[3], d[3]));
+		ASSERT_EQ( -1, NextDate( y[i], m[3], d[4]));
+		
+		ASSERT_EQ( (y[i]*10000 + m[0]*100 + d[3] + 1), NextDate( y[i], m[4], d[3]));
+		ASSERT_EQ( ((y[i]+1)*10000 + 100 + 1), NextDate( y[i], m[4], d[4]));
+	}
+}
