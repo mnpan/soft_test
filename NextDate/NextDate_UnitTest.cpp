a1080918828ed582dbeaee46662279c7fa117b2c
mnpan <sa69mo@gmail.com>
2017-03-19 23:16:40 +0800

    NextDate Decision Table: normal test pass

--- NextDate/NextDate_UnitTest.cpp
+++ NextDate/NextDate_UnitTest.cpp
@@ -1,6 +1,7 @@
 #include "NextDate.cpp"
 //#include "NextDate_BoundaryValueTest.cpp"
 //#include "NextDate_EquivalenceClass.cpp"
-#include "NextDate_Edge.cpp"
+//#include "NextDate_Edge.cpp"
+#include "NextDate_DecisionTable.cpp"
 
 
