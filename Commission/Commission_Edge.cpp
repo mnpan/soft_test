c8b22ebcce61ea92d5ac117042a0513efb26de42
mnpan <sa69mo@gmail.com>
2017-03-20 20:00:03 +0800

    Commission Edge: Normal test T2 pass

--- Commission/Commission_Edge.cpp
+++ Commission/Commission_Edge.cpp
@@ -13,17 +13,31 @@ TEST( ComEdge, Normal){
 	}
 	
 	/*** T1: (1)C1+C1+C1 (2)C2/C3+C1+C1 ***/
-	for( int i = 0; i <= 2; i++){
-		for( int j = 0; j <= 2; j++){
-			for( int k = 0; k <= 2; k++){
+	for( int i = 0; i < 2; i++){
+		for( int j = 0; j < 2; j++){
+			for( int k = 0; k < 2; k++){
 				ASSERT_EQ( (l[i]*45 + s[j]*30 + b[k]*25)*0.1, Commission( l[i], s[j], b[k]));
 			}
+			ASSERT_EQ( (l[2]*45 + s[i]*30 + b[j]*25)*0.1, Commission( l[2], s[i], b[j]));
 			ASSERT_EQ( (l[3]*45 + s[i]*30 + b[j]*25)*0.1, Commission( l[3], s[i], b[j]));
-			ASSERT_EQ( (l[4]*45 + s[i]*30 + b[j]*25)*0.1, Commission( l[4], s[i], b[j]));
+			ASSERT_EQ( (l[i]*45 + s[2]*30 + b[j]*25)*0.1, Commission( l[i], s[2], b[j]));
 			ASSERT_EQ( (l[i]*45 + s[3]*30 + b[j]*25)*0.1, Commission( l[i], s[3], b[j]));
-			ASSERT_EQ( (l[i]*45 + s[4]*30 + b[j]*25)*0.1, Commission( l[i], s[4], b[j]));
+			ASSERT_EQ( (l[i]*45 + s[j]*30 + b[2]*25)*0.1, Commission( l[i], s[j], b[2]));
 			ASSERT_EQ( (l[i]*45 + s[j]*30 + b[3]*25)*0.1, Commission( l[i], s[j], b[3]));
-			ASSERT_EQ( (l[i]*45 + s[j]*30 + b[4]*25)*0.1, Commission( l[i], s[j], b[4]));
 		}
 	}
+	
+	/*** T2: (1)C2/C3+C2/C3+C1 (2)C2+C2+C2 ***/
+	for( int i = 2; i <= 3; i++){
+		for( int j = 2; j <= 3; j++){
+			for( int k = 0; k < 2; k++){
+				ASSERT_EQ( 100 + ((l[i]*45 + s[j]*30 + b[k]*25)-1000)*0.15, Commission( l[i], s[j], b[k]));
+				ASSERT_EQ( 100 + ((l[i]*45 + s[k]*30 + b[j]*25)-1000)*0.15, Commission( l[i], s[k], b[j]));
+				ASSERT_EQ( 100 + ((l[k]*45 + s[i]*30 + b[j]*25)-1000)*0.15, Commission( l[k], s[i], b[j]));
+			}
+		}
+	}
+	ASSERT_EQ( 100 + ((l[2]*45 + s[2]*30 + b[2]*25)-1000)*0.15, Commission( l[2], s[2], b[2]));
+	
+	
 }
