7a354caa9ab1d16635fed37b48c2ecc66a5d69c8
mnpan <sa69mo@gmail.com>
2017-03-20 17:40:44 +0800

    Commission Equivalence Class: StrongRobust test pass

--- Commission/Commission_EquivalenceClass.cpp
+++ Commission/Commission_EquivalenceClass.cpp
@@ -44,3 +44,60 @@ TEST( ComEquivalence, WeakRobust){
 	ASSERT_EQ( OUT_RANGE, Commission( RVl[2], NVs[1], NVb[0]));
 	
 }
+
+TEST( ComEquivalence, StrongRobust){
+	int NVl[3] = { 15, 60, -1};
+	int NVs[2] = { 20, 60};
+	int NVb[2] = { 25, 70};
+	
+	int RVl[3] = { -2, 0, 71};
+	int RVs[2] = { 0, 81};
+	int RVb[2] = { 0, 91};
+	
+	/*** -1 terminal program ***/
+	for( int j = 0; j < 2; j++){
+		for( int k = 0; k < 2; k++){
+			ASSERT_EQ( TERMINAL, Commission( NVl[2], NVs[j], NVb[k]));
+			ASSERT_EQ( TERMINAL, Commission( NVl[2], RVs[j], NVb[k]));
+			ASSERT_EQ( TERMINAL, Commission( NVl[2], NVs[j], RVb[k]));
+			ASSERT_EQ( TERMINAL, Commission( NVl[2], RVs[j], RVb[k]));
+		}
+	}
+	
+	/*** one robust value ***/
+	for( int j = 0; j < 2; j++){
+		for( int k = 0; k < 2; k++){
+			ASSERT_EQ( OUT_RANGE, Commission( RVl[0], NVs[j], NVb[k]));
+			ASSERT_EQ( OUT_RANGE, Commission( RVl[1], NVs[j], NVb[k]));
+			ASSERT_EQ( OUT_RANGE, Commission( RVl[2], NVs[j], NVb[k]));
+			
+			for( int i = 0; i < 2; i++){
+				ASSERT_EQ( OUT_RANGE, Commission( NVl[j], RVs[i], NVb[k]));
+				ASSERT_EQ( OUT_RANGE, Commission( NVl[j], NVs[i], RVb[0]));
+			}
+		}
+	}
+	
+	/*** two robust value ***/
+	for( int j = 0; j < 2; j++){
+		for( int k = 0; k < 2; k++){
+			for( int i = 0; i < 3; i++){
+				ASSERT_EQ( OUT_RANGE, Commission( RVl[i], RVs[j], NVb[k]));
+				ASSERT_EQ( OUT_RANGE, Commission( RVl[i], NVs[j], RVb[k]));
+			}
+			for( int i = 0; i < 2; i++){
+				ASSERT_EQ( OUT_RANGE, Commission( NVl[i], RVs[j], RVb[k]));
+				ASSERT_EQ( OUT_RANGE, Commission( NVl[i], RVs[k], RVb[k]));
+			}
+		}
+	}
+	
+	/*** three robust value ***/
+	for( int j = 0; j < 2; j++){
+		for( int k = 0; k < 2; k++){
+			for( int i = 0; i < 3; i++){
+				ASSERT_EQ( OUT_RANGE, Commission( RVl[i], RVs[j], RVb[k]));
+			}
+		}
+	}
+}
