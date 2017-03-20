cdbb870a42217cce2c506ae9f633a6982832a7cf
mnpan <sa69mo@gmail.com>
2017-03-20 15:33:53 +0800

    Commission Boundary Value: Normal test error

--- main_test.cpp
+++ main_test.cpp
@@ -1,5 +1,6 @@
 //#include "Triangle/Triangle_UnitTest.cpp"
-#include "NextDate/NextDate_UnitTest.cpp"
+//#include "NextDate/NextDate_UnitTest.cpp"
+#include "Commission/Commission_UnitTest.cpp"
 #include <gtest/gtest.h>
 
 int main( int argc, char** argv){
