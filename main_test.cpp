a037fb7d3880ec58e1259d37f576792189cf8e0c
mnpan <sa69mo@gmail.com>
2017-05-01 19:33:21 +0800

    Triangle: MCDC:Pass, Coverage

--- main_test.cpp
+++ main_test.cpp
@@ -1,6 +1,9 @@
-#include "Triangle/Triangle_UnitTest.cpp"
-#include "NextDate/NextDate_UnitTest.cpp"
-#include "Commission/Commission_UnitTest.cpp"
+//#include "Triangle/Triangle_UnitTest.cpp"
+//#include "NextDate/NextDate_UnitTest.cpp"
+//#include "Commission/Commission_UnitTest.cpp"
+#include "Triangle/Triangle_PathTest.cpp"
+//#include "NextDate/NextDate_UnitTest.cpp"
+//#include "Commission/Commission_UnitTest.cpp"
 #include <gtest/gtest.h>
 
 int main( int argc, char** argv){
