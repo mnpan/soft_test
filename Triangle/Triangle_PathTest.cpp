e9d834170bec49b0c020b611d83d8bb0902d2c0b
mnpan <sa69mo@gmail.com>
2017-05-01 19:33:06 +0800

    Triangle: MCDC:write testcase

--- Triangle/Triangle_PathTest.cpp
+++ Triangle/Triangle_PathTest.cpp
@@ -8,3 +8,14 @@ TEST( TriPath, C0){
 	ASSERT_EQ( ISOSCELES, Triangle( 100, 100, 80));
 	ASSERT_EQ( NON_TRIANGLE, Triangle( 60, 60, 130));
 }
+
+TEST( TriPath, MCDC){
+	ASSERT_EQ( EQUILATERAL, Triangle( 100, 100, 100));
+	ASSERT_EQ( OUT_RANGE, Triangle( 100, 100, 0));
+	ASSERT_EQ( OUT_RANGE, Triangle( 100, 0, 100));
+	ASSERT_EQ( OUT_RANGE, Triangle( 0, 100, 100));
+	ASSERT_EQ( SCALENE, Triangle( 90, 120, 150));
+	ASSERT_EQ( NON_TRIANGLE, Triangle( 90, 90, 190));
+	ASSERT_EQ( NON_TRIANGLE, Triangle( 90, 190, 90));
+	ASSERT_EQ( NON_TRIANGLE, Triangle( 190, 90, 90));
+}
