69f3d02c4bbcf0ef923ddbdb54bff50c809aef79
mnpan <sa69mo@gmail.com>
2017-05-03 16:30:17 +0800

    NextDate: MCDC: testcase error

--- NextDate/NextDate_PathTest.cpp
+++ NextDate/NextDate_PathTest.cpp
@@ -1,4 +1,5 @@
 #include <gtest/gtest.h>
+//#include <iostream>
 #include "NextDate.cpp"
 
 TEST( NextPath, C0){
@@ -21,4 +22,14 @@ TEST( NextPath, C1){
 	ASSERT_EQ( INVALID_DATE, NextDate( 1995, 2, 29));
 	ASSERT_EQ( 19950216, NextDate( 1995, 2, 15));
 	ASSERT_EQ( INVALID_DATE, NextDate( 1995, 14, 31));
+}
+
+TEST( NextPath, MCDC){
+	ASSERT_EQ( 19950618, NextDate( 1995, 6, 17));
+	ASSERT_EQ( INVALID_DATE, NextDate( 1995, 13, 17));
+	ASSERT_EQ( INVALID_DATE, NextDate( 2020, 6, 17)) << "Condition1 END";
+	
+	ASSERT_EQ( INVALID_DATE, NextDate( 1995, 6, 31));
+	ASSERT_EQ( 19950616, NextDate( 1995, 6, 16));
+	ASSERT_EQ( 19950801, NextDate( 1995, 7, 31)) << "Condition2 END";
 }
