b3211ab177e6dc06aaf6023171ddd1d8fc2ca659
mnpan <sa69mo@gmail.com>
2017-05-03 15:46:04 +0800

    Commission: MCDC: testcase testOK

--- Commission/Commission_PathTest.cpp
+++ Commission/Commission_PathTest.cpp
@@ -10,3 +10,10 @@ TEST( ComPath, C0){
 	ASSERT_EQ( 160.75, Commission( 30,1,1));
 	ASSERT_EQ( 460, Commission( 30,30,30));
 }
+
+TEST( ComPath, MCDC){
+	ASSERT_EQ( 460, Commission( 30,30,30));
+	ASSERT_EQ( OUT_RANGED, Commission( 30,30,91));
+	ASSERT_EQ( OUT_RANGED, Commission( 30,81,30));
+	ASSERT_EQ( OUT_RANGED, Commission( 71,30,30));
+}
